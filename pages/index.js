e379d89e35b03de3479da5ed3f01d5c702793a70
ayshasiddiqua513 <22it026@skcet.ac.in>
2024-06-05 21:40:02 +0530

    Prerendering the powers of Next JS

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,40 @@ const DUMMY_MEETUPS = [
       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
       address: 'Some address 10, 12345 Some City',
       description: 'This is a second meetup!'
+    },
+    {
+      id: 'm3',
+      title: 'A Third Meetup',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Some address 15, 12345 Some City',
+      description: 'This is a third meetup!'
     }
   ];
 
-function HomePage(){
+function HomePage(props){
     return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={props.meetups} />
     );
+}
+
+// export async function getServerSideProps(context){
+//     const req=context.req;
+//     const res=context.res;
+
+//     return{
+//         props:{
+//             meetups: DUMMY_MEETUPS
+//         }
+//     };
+// }
+
+export async function getStaticProps() {
+    return {
+        props: {
+            meetups:DUMMY_MEETUPS
+        },
+        revalidate: 10
+    };
 }
 
 export default HomePage;
